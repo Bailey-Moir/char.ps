5af5c4bd8666d16ffd329a051a4c39db75a1d889
Bailey Moir <bailey.p.moir@gmail.com>
2024-07-02 21:22:45 +1200

    stack overflow

--- app/components/link-box/index.tsx
+++ app/components/link-box/index.tsx
@@ -3,9 +3,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExternalLink } from "@fortawesome/free-solid-svg-icons";
 
 
-export default function LinkBox({ href, text, icon, className}: { href: string, text: string, icon: string, className?: string}) {
+export default function LinkBox({ href, text, icon, className, white}: { href: string, text: string, icon: string, className?: string, white?: boolean}) {
     return <a href={href} className={className + ' ' + styles.container}>
-        <img src={icon} />
+        <div className={styles.cropper} style={ white ? {'background': 'white'} : {}}>
+            <img src={icon} />
+        </div>
         <div className={styles.inner}>
             <p className={`${styles.name} font-mono`}>
                 {text}
--- app/links/page.tsx
+++ app/links/page.tsx
@@ -10,8 +10,7 @@ export default function Page() {
     //useEffect(() => {
     //    if (!canvasRef.current) return;
     //    const ctx = canvasRef.current.getContext('2d');
-    //    // Load image
-    //    const image = new Image();
+    //    // Load image const image = new Image();
     //    image.onload = () => {
     //      // Draw the image into the canvas
     //      ctx.drawImage(image, 0, 0);
@@ -25,9 +24,10 @@ export default function Page() {
             <img src="/racoon.png" />            
         </div>
         <div className="links">
-            <LinkBox href="https://github.com/Bailey-Moir/" text="Github" icon="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png" />    
-            <LinkBox href="https://www.upwork.com/freelancers/~01115c08d3953297a7?viewMode=1" text="Upwork" icon="https://yt3.googleusercontent.com/Rt0a-mNaBF7D9fzrf0sHhbJr0PZT0nftaII_lWzWJ1Oqnj20ovwsKEMmlQfq-H1pm8kgFBwDDag=s900-c-k-c0x00ffffff-no-rj" />    
-            <LinkBox href="https://open.spotify.com/user/bpmpop" text="Spotify" icon="https://yt3.googleusercontent.com/vuOdWtsiJ02ciel4pqaheZbl3SJx5uP5xu_xJlAilwFRKsvYjZqHGiIGvZxWKVHIEHvVRhQctrc=s900-c-k-c0x00ffffff-no-rj" />    
+            <LinkBox href="https://github.com/Bailey-Moir/" text="Github" icon="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png" />
+            <LinkBox href="https://www.upwork.com/freelancers/~01115c08d3953297a7?viewMode=1" text="Upwork" icon="https://yt3.googleusercontent.com/Rt0a-mNaBF7D9fzrf0sHhbJr0PZT0nftaII_lWzWJ1Oqnj20ovwsKEMmlQfq-H1pm8kgFBwDDag=s900-c-k-c0x00ffffff-no-rj" />
+            <LinkBox white={true} href="https://stackoverflow.com/users/13525249/charps" text="Stack Overflow" icon="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Stack_Overflow_icon.svg/768px-Stack_Overflow_icon.svg.png" />
+            <LinkBox href="https://open.spotify.com/user/bpmpop" text="Spotify" icon="https://yt3.googleusercontent.com/vuOdWtsiJ02ciel4pqaheZbl3SJx5uP5xu_xJlAilwFRKsvYjZqHGiIGvZxWKVHIEHvVRhQctrc=s900-c-k-c0x00ffffff-no-rj" />
         </div>
     </>
 }
