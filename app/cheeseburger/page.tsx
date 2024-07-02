04256f72a5b7a60556fbe3e08d1429588c8bd66b
Bailey Moir <bailey.p.moir@gmail.com>
2024-07-02 21:56:31 +1200

    burger

---
+++ app/cheeseburger/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import './styles.scss';
+//import React, { useRef, useEffect } from 'react';
+
+export default function Page() {
+    return <img src="/cheeseburger.svg" alt="cheesburger" />
+}
