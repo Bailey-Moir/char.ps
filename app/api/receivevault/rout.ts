acaf4578811e551999ccb5eb9c175b11d607b244
Bailey-Moir <bailey.p.moir@gmail.com>
2024-07-18 18:51:16 +1200

    returning api

---
+++ app/api/receivevault/rout.ts
@@ -0,0 +1,22 @@
+import {NextResponse} from "next/server";
+
+export async function POST(req: Request) {
+    // Authorization
+    //try {
+    //    if (req.headers.get('authorization').split(' ')[1] != getEnv('ZENDESK_WEBHOOK_TOKEN')) {
+    //        console.log('Error 401: incorrect bearer token.');
+    //        return NextResponse.json(null, {status: 401});
+    //    }
+    //} catch {
+    //    console.log('Error 401: authorization header formatted incorrectly or missing.');
+    //    return NextResponse.json(null, {status: 400});
+    //}
+
+    // Parse body JSON
+    return new NextResponse(req.body);
+    try {
+        req.body
+    } catch {
+        return NextResponse.json({error: 'Request JSON parse failed'}, {status: 400});
+    }
+}
