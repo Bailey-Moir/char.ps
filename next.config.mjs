0455d4662b1cfe632a37c90f2bc822280b123e89
Bailey-Moir <bailey.p.moir@gmail.com>
2024-07-19 21:29:11 +1200

    others

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,50 @@
 const nextConfig = {
     async rewrites() {
         return {
+            beforeFiles: [
+                {
+                    source: "/",
+                    destination: "https://publish.obsidian.md/serve?url=www.vlt.char.ps/",
+                    has: [
+                        {
+                            type: "host",
+                            value: "www.vlt.char.ps"
+                        }
+                    ]
+                },
+                {
+                    source: "/:path*",
+                    destination: "https://publish.obsidian.md/serve?url=www.vlt.char.ps/:path*",
+                    has: [
+                        {
+                            type: "host",
+                            value: "www.vlt.char.ps"
+                        }
+                    ]
+                }
+            ],
+            afterFiles: [
+                {
+                    source: "/",
+                    destination: "https://publish.obsidian.md/serve?url=www.vlt.char.ps/",
+                    has: [
+                        {
+                            type: "host",
+                            value: "www.vlt.char.ps"
+                        }
+                    ]
+                },
+                {
+                    source: "/:path*",
+                    destination: "https://publish.obsidian.md/serve?url=www.vlt.char.ps/:path*",
+                    has: [
+                        {
+                            type: "host",
+                            value: "www.vlt.char.ps"
+                        }
+                    ]
+                }
+            ],
             fallback: [
                 {
                     source: "/",
