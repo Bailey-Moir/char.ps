b4b3b85572b3605a83f670a146868807b6338caf
Bailey-Moir <bailey.p.moir@gmail.com>
2024-07-19 21:20:01 +1200

    next.config/mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: "/",
+                destination: "https://publish.obsidian.md/serve?url=www.vlt.char.ps/",
+                has: [
+                    {
+                        type: "host",
+                        value: "www.vlt.char.ps"
+                    }
+                ]
+            },
+            {
+                source: "/:path*",
+                destination: "https://publish.obsidian.md/serve?url=www.vlt.char.ps/:path*",
+                has: [
+                    {
+                        type: "host",
+                        value: "www.vlt.char.ps"
+                    }
+                ]
+            }
+        ]
+    },
+};
 
 export default nextConfig;
+
+
